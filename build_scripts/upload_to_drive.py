71a7b151ee780d3887f1f46d7918c64b8ec65e21
THEPPRATAN NAKKAEW <theppratan.dev@gmail.com>
2026-08-13 20:35:48 +0700

    fix: use drive scope instead of drive.file for shared folder access [build]

--- build_scripts/upload_to_drive.py
+++ build_scripts/upload_to_drive.py
@@ -57,7 +57,7 @@ def authenticate():
 
     creds = service_account.Credentials.from_service_account_info(
         creds_info,
-        scopes=['https://www.googleapis.com/auth/drive.file']
+        scopes=['https://www.googleapis.com/auth/drive']
     )
     return build('drive', 'v3', credentials=creds)
 
